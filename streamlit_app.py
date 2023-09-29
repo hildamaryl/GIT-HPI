aabe4dc2f6de0afd19e3708cb1bea480433ee6d9
hildamaryl <146208873+hildamaryl@users.noreply.github.com>
2023-09-29 13:35:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+Breakfast Menu
+
+Omega 3 & Blueberry Oatmeal
+
+Kale, spinach and Rocket Smothie
+
+Hard-Boiled Free-Range Egg
