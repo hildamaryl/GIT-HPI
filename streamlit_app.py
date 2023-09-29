60dce312a8eadf1ecf1c5cae866dccfa5f3208d5
hildamaryl <146208873+hildamaryl@users.noreply.github.com>
2023-09-29 14:34:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.header(Breakfast Menu')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 'avacado test')  
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado test')  
  
