f30fb295b3f0561dda0d4e599cc67a819ace6ffe
hildamaryl <146208873+hildamaryl@users.noreply.github.com>
2023-10-20 18:49:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,17 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
  
 
 streamlit.title("My Mom's New Healthy Diner")
-streamlit.header('🥣 Breakfast favorites')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard- Boiled Free - Range Egg')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard- Boiled Free - Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
  
 
@@ -14,9 +19,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
  
 
- 
-
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -34,7 +37,7 @@ streamlit.dataframe(my_fruit_list)
 
  
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -46,7 +49,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
  
@@ -55,3 +58,32 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+ 
+
+#don't run anything past here while troubleshoot 
+streamlit.stop()
+
+ 
+
+#import snowflake.connector
+
+ 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+ 
+
+#Allow the end user  to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+ 
+
+#This will not work correnctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values (' from streamlit')")
